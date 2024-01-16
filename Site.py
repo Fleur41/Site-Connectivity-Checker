2af915491d772d97fe07f63bd8e3441c4a91d0c6
Fleur41 <Muriithikings456@gmail.com>
2024-01-16 13:36:13 +0300

    test_connectivity

--- Site.py
+++ Site.py
@@ -3,3 +3,12 @@ Site Connectivity Checker
 -------------------------------------------------------------
 Enter websites as http(s)://www.yourwebsite.com
 '''
+
+import urllib.request
+import tkinter as tk
+
+def test_connectivity():
+    window = tk.Tk()
+    window.geometry('600x400')
+    head = tk.Label(window, text='Website Connectivity Checker',
+                  font=('Calibri 15'))
